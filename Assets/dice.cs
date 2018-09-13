f2e0f0ba1c82528d49a8e01780287c55cb3ffbac
MK <falsetea@gmail.com>
2018-09-14 06:15:44 +0900

    Add dice.cs & pythonwrapper.cs

---
+++ Assets/dice.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using pythonwrapper;
+using System;
+
+class Dice
+{
+    public int roll(int times = 1, int sided = 6)
+    {
+        if (sided <= 1) return times;
+        int n = 0;
+        for (int i = 0; i < times; i++)
+        {
+            n += (int)(py_random.random() * sided) + 1; // TODO: python package
+        }
+        return n;
+    }
+
+    public T? choice<T>(List<T> seq) where T : struct
+    {
+        // TODO: Nullableも扱う必要があるかもしれない
+        if (seq.Count == 0)
+        {
+            return null;
+        }
+        return py_random.choice(seq);
+    }
+
+    public List<T> shuffle<T>(List<T> seq)
+    {
+        List<T> seq2 = py_copy.copy(seq);
+        py_random.shuffle(seq2);
+        return seq2;
+    }
+
+    public T? pop<T>(List<T> seq) where T : struct
+    {
+        // TODO: Nullableも扱う必要があるかもしれない
+        T? item = choice<T>(seq);
+
+        if (item != null)
+        {
+            seq.Remove((T)item);
+        }
+
+        return item;
+    }
+}
---
+++ Assets/pythonwrapper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace pythonwrapper
+{
+    static class py_random
+    {
+        public static float random()
+        {
+            // 注意: python のrandom.randomは[min,max)、UnityのRandom.Rangeは[min,max]
+            return Random.Range(0.0f, 0.999999999f);
+        }
+
+        public static T choice<T>(List<T> seq)
+        {
+            return seq[Random.Range(0, seq.Count)];
+        }
+
+
+        public static void shuffle<T>(this List<T> seq)
+        {
+            for (int i = 0; i < seq.Count; i++)
+            {
+                T temp = seq[i];
+                int index = Random.Range(0, seq.Count);
+                seq[i] = seq[index];
+                seq[index] = temp;
+            }
+        }
+    }
+
+    static class py_copy
+    {
+        public static List<T> copy<T>(List<T> seq)
+        {
+            List<T> seq2 = new List<T>();
+            for(int i=0; i<seq.Count; i++)
+            {
+                seq2.Add(seq[i]);
+            }
+            return seq2;
+        }
+    }
+}
